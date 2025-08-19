b9db7c9d5aca9ac23d7fe6c3120a6dcb89386707
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:51:03 +0530

    Add pro83.py

--- pro83.py
+++ pro83.py
@@ -1,9 +1,1 @@
-# Radiating Arcs
-# Auto-generated turtle graphics demo #83
-import turtle as t
-t.speed(0)
-for i in range(36):
-    t.circle(120, 180)
-    t.right(10)
-t.hideturtle(); t.done()
 # Program 83
