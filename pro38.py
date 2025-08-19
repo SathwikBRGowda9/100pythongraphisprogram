582e7789f216e2f5121af7cba527d95cf01dcacb
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:37 +0530

    Add pro38.py

--- pro38.py
+++ pro38.py
@@ -1,10 +1,1 @@
-# Triangle Spiral
-# Auto-generated turtle graphics demo #38
-import turtle as t
-t.speed(0)
-d=5
-for _ in range(120):
-    t.forward(d); t.right(120)
-    d += 4
-t.hideturtle(); t.done()
 # Program 38
