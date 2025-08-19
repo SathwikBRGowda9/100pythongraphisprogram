334508c09cb0ac4a3657bce55be5540dda97ed2d
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:51:01 +0530

    Add pro79.py

--- pro79.py
+++ pro79.py
@@ -1,10 +1,1 @@
-# Polygon Spiral (sides=5) #79
-# Auto-generated turtle graphics demo #79
-import turtle as t
-t.speed(0)
-d=4
-for _ in range(150):
-    t.forward(d); t.right(360/5)
-    d += 2
-t.hideturtle(); t.done()
 # Program 79
