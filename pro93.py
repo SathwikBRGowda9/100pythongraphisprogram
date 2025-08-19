5d6015d378a8feef98c85b3c5360420a05bdf5b2
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:51:09 +0530

    Add pro93.py

--- pro93.py
+++ pro93.py
@@ -1,11 +1,1 @@
-# Creative Combo #93
-# Auto-generated turtle graphics demo #93
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(24):
-    for _ in range(6):
-        t.forward(100); t.right(360/6)
-    t.circle(60, 120)
-    t.right(39)
-t.hideturtle(); t.done()
 # Program 93
