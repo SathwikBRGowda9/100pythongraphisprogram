b85a34bd75a27d9505a58f6b627e82f30ddd6aef
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:51:05 +0530

    Add pro87.py

--- pro87.py
+++ pro87.py
@@ -1,15 +1,1 @@
-# Double Spiral
-# Auto-generated turtle graphics demo #87
-import turtle as t
-t.speed(0)
-d=5
-for _ in range(90):
-    t.forward(d); t.right(89)
-    d += 3
-t.left(180)
-d=5
-for _ in range(90):
-    t.forward(d); t.right(89)
-    d += 3
-t.hideturtle(); t.done()
 # Program 87
