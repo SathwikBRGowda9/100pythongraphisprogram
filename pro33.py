ff9287e36734306e3ce8fc368fbb80ece0c3b9f9
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:34 +0530

    Add pro33.py

--- pro33.py
+++ pro33.py
@@ -1,10 +1,1 @@
-# Spirograph Circles
-# Auto-generated turtle graphics demo #33
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(60):
-    t.circle(120)
-    t.right(6)
-t.hideturtle()
-t.done()
 # Program 33
