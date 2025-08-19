d5d53e309a92241875517ede738e3768387a6be4
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:31 +0530

    Add pro27.py

--- pro27.py
+++ pro27.py
@@ -1,11 +1,1 @@
-# Radial Polygons #27
-# Auto-generated turtle graphics demo #27
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(24):
-    for _ in range(9):
-        t.forward(100); t.right(360/9)
-    t.right(360/24)
-t.hideturtle()
-t.done()
 # Program 27
