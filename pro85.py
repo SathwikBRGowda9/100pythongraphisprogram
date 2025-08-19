cb39ebd616e19b3a2eb1c947ace7bccf2f059b1f
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:51:04 +0530

    Add pro85.py

--- pro85.py
+++ pro85.py
@@ -1,11 +1,1 @@
-# Wavy Square
-# Auto-generated turtle graphics demo #85
-import turtle as t
-t.speed(0)
-for i in range(4):
-    for _ in range(18):
-        t.circle(20, 20)
-        t.circle(-20, 20)
-    t.right(90)
-t.hideturtle(); t.done()
 # Program 85
