3e28cc4386077c190b9362740ca24a2ac05bae52
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:34 +0530

    Add pro32.py

--- pro32.py
+++ pro32.py
@@ -1,10 +1,1 @@
-# Yin Yang Simple
-# Auto-generated turtle graphics demo #32
-import turtle as t
-t.speed(0); t.width(2)
-t.circle(100,180); t.circle(50,180)
-t.left(180); t.circle(50,180); t.circle(100,180)
-t.penup(); t.goto(0,100); t.pendown(); t.circle(10)
-t.penup(); t.goto(0,-100); t.pendown(); t.circle(10)
-t.hideturtle(); t.done()
 # Program 32
