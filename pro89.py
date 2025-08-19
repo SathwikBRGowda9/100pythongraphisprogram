9ceaf49b5c3b0d080da92009e64a1b921f7b906a
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:51:06 +0530

    Add pro89.py

--- pro89.py
+++ pro89.py
@@ -1,10 +1,1 @@
-# Heart Rosette
-# Auto-generated turtle graphics demo #89
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(18):
-    t.left(45); t.forward(100); t.circle(50, 200)
-    t.left(160); t.circle(50, 200); t.forward(100); t.right(205)
-    t.right(20)
-t.hideturtle(); t.done()
 # Program 89
