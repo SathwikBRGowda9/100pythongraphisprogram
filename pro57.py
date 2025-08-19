a0d6c8eff83102f40b5420f8d32b732192a2e0fb
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:48 +0530

    Add pro57.py

--- pro57.py
+++ pro57.py
@@ -1,10 +1,1 @@
-# Star Rotation #57
-# Auto-generated turtle graphics demo #57
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(36):
-    for _ in range(5):
-        t.forward(120); t.right(144)
-    t.right(45)
-t.hideturtle(); t.done()
 # Program 57
