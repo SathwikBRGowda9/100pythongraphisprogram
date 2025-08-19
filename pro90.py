bf49d229abcd1294ed27904da23bdbf21462634f
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:51:07 +0530

    Add pro90.py

--- pro90.py
+++ pro90.py
@@ -1,9 +1,1 @@
-# Gear Teeth
-# Auto-generated turtle graphics demo #90
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(36):
-    t.forward(30); t.right(30); t.forward(30); t.left(30)
-    t.right(10)
-t.hideturtle(); t.done()
 # Program 90
