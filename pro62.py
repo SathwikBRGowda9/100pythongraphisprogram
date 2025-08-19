295ba30e42815f2dd916e0d1ffb50652d6297578
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:51 +0530

    Add pro62.py

--- pro62.py
+++ pro62.py
@@ -1,13 +1,1 @@
-# Rose Curve 6 Petals
-# Auto-generated turtle graphics demo #62
-import turtle as t, math
-t.speed(0)
-t.penup()
-for a in range(0, 361*4, 2):
-    k=3
-    r = 150*math.sin(math.radians(k*a))
-    x = r*math.cos(math.radians(a))
-    y = r*math.sin(math.radians(a))
-    t.goto(x,y); t.pendown()
-t.hideturtle(); t.done()
 # Program 62
