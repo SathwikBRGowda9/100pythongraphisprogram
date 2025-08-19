16d24c43d08578e4542a1755522f897ca5a76473
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:51:05 +0530

    Add pro86.py

--- pro86.py
+++ pro86.py
@@ -1,10 +1,1 @@
-# Starry Dots Spiral
-# Auto-generated turtle graphics demo #86
-import turtle as t, math
-t.speed(0)
-for i in range(160):
-    r=3*i; a=11*i
-    t.penup(); t.goto(r*math.cos(math.radians(a)), r*math.sin(math.radians(a))); t.pendown()
-    t.dot(6)
-t.hideturtle(); t.done()
 # Program 86
