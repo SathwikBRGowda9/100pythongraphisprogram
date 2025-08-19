080364bfdbd59cd4acb5afccb4a9c9c3c7191080
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:51:09 +0530

    Add pro94.py

--- pro94.py
+++ pro94.py
@@ -1,11 +1,1 @@
-# Creative Combo #94
-# Auto-generated turtle graphics demo #94
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(24):
-    for _ in range(7):
-        t.forward(100); t.right(360/7)
-    t.circle(60, 120)
-    t.right(52)
-t.hideturtle(); t.done()
 # Program 94
