dc89579ba1283449fd9b21dfdff40ea404eeddcb
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:18 +0530

    Add pro3.py

--- pro3.py
+++ pro3.py
@@ -1,11 +1,1 @@
-# Spiral Square
-# Auto-generated turtle graphics demo #3
-import turtle as t
-t.speed(0)
-d=5
-for _ in range(120):
-    t.forward(d); t.right(90)
-    d += 3
-t.hideturtle()
-t.done()
 # Program 3
