f69020c51b6d0e8d853d1b0c77e8f77604073a48
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:33 +0530

    Add pro31.py

--- pro31.py
+++ pro31.py
@@ -1,10 +1,1 @@
-# Circle Petal Flower
-# Auto-generated turtle graphics demo #31
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(18):
-    t.circle(120)
-    t.right(20)
-t.hideturtle()
-t.done()
 # Program 31
