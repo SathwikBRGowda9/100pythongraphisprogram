b38ce8bd3c38725603d481892f894e4980469bdd
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:22 +0530

    Add pro11.py

--- pro11.py
+++ pro11.py
@@ -1,11 +1,1 @@
-# Radial Polygons #11
-# Auto-generated turtle graphics demo #11
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(24):
-    for _ in range(7):
-        t.forward(100); t.right(360/7)
-    t.right(360/24)
-t.hideturtle()
-t.done()
 # Program 11
