cb1d6f7e566d8163a2f8f856ab7a1a3c111079cf
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:58 +0530

    Add pro75.py

--- pro75.py
+++ pro75.py
@@ -1,10 +1,1 @@
-# Polygon Spiral (sides=8) #75
-# Auto-generated turtle graphics demo #75
-import turtle as t
-t.speed(0)
-d=4
-for _ in range(150):
-    t.forward(d); t.right(360/8)
-    d += 2
-t.hideturtle(); t.done()
 # Program 75
