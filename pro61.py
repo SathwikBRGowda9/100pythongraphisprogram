1e0bbfe13f9308a57845925baae88ecb5b3018a1
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:51 +0530

    Add pro61.py

--- pro61.py
+++ pro61.py
@@ -1,11 +1,1 @@
-# Circle Grid
-# Auto-generated turtle graphics demo #61
-import turtle as t
-t.speed(0)
-start_x, start_y = -220, 220
-for r in range(7):
-    for c in range(7):
-        t.penup(); t.goto(start_x+c*70, start_y-r*70-35); t.pendown()
-        t.circle(35)
-t.hideturtle(); t.done()
 # Program 61
