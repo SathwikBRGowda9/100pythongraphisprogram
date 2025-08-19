e9fec65f4aa427d7dcb7b05a534d6c283f120ff0
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:41 +0530

    Add pro44.py

--- pro44.py
+++ pro44.py
@@ -1,10 +1,1 @@
-# Square Spiral Variant #44
-# Auto-generated turtle graphics demo #44
-import turtle as t
-t.speed(0)
-d=4
-for _ in range(150):
-    t.forward(d); t.right(128)
-    d += 2
-t.hideturtle(); t.done()
 # Program 44
