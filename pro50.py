ac38484ddf392e1038376820c8bd1762e4ca04db
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:44 +0530

    Add pro50.py

--- pro50.py
+++ pro50.py
@@ -1,10 +1,1 @@
-# Square Spiral Variant #50
-# Auto-generated turtle graphics demo #50
-import turtle as t
-t.speed(0)
-d=4
-for _ in range(150):
-    t.forward(d); t.right(170)
-    d += 2
-t.hideturtle(); t.done()
 # Program 50
