0aa3746c8383a4c2c39d292203a04db4a351ced4
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:39 +0530

    Add pro41.py

--- pro41.py
+++ pro41.py
@@ -1,10 +1,1 @@
-# Square Spiral Variant #41
-# Auto-generated turtle graphics demo #41
-import turtle as t
-t.speed(0)
-d=4
-for _ in range(150):
-    t.forward(d); t.right(107)
-    d += 2
-t.hideturtle(); t.done()
 # Program 41
