72c0ff13bb07b58d78c605b056a50ab532a43bec
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:29 +0530

    Add pro23.py

--- pro23.py
+++ pro23.py
@@ -1,11 +1,1 @@
-# Radial Polygons #23
-# Auto-generated turtle graphics demo #23
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(24):
-    for _ in range(5):
-        t.forward(100); t.right(360/5)
-    t.right(360/24)
-t.hideturtle()
-t.done()
 # Program 23
