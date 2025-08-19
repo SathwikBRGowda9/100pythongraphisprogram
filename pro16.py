9eed63b4fe5af21095c39e687228e6717d7af3d4
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:50:25 +0530

    Add pro16.py

--- pro16.py
+++ pro16.py
@@ -1,11 +1,1 @@
-# Radial Polygons #16
-# Auto-generated turtle graphics demo #16
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(24):
-    for _ in range(5):
-        t.forward(100); t.right(360/5)
-    t.right(360/24)
-t.hideturtle()
-t.done()
 # Program 16
