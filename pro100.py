651faa921fe75901c794f7eb607f946c4b8761ee
SathwikBRGowda9 <sathwikbrgowda8234@gmail.com>
2025-08-19 17:51:13 +0530

    Add pro100.py

--- pro100.py
+++ pro100.py
@@ -1,11 +1,1 @@
-# Creative Combo #100
-# Auto-generated turtle graphics demo #100
-import turtle as t
-t.speed(0); t.width(2)
-for i in range(24):
-    for _ in range(3):
-        t.forward(100); t.right(360/3)
-    t.circle(60, 120)
-    t.right(40)
-t.hideturtle(); t.done()
 # Program 100
